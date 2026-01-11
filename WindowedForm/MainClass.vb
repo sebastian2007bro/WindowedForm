7c77ac8060d3d7093c1121a59c00d0313cc72dd8
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2026-01-11 02:39:59 +0100

    Added source code

---
+++ WindowedForm/MainClass.vb
@@ -0,0 +1,33 @@
+﻿Imports TouchTest
+Imports System.Windows.Forms
+
+Public Class MainClass
+    Implements TouchTest.Q_U_U_U_Q_I
+
+    Public Shared Form1 As Form1
+    Public Shared UI As UI
+
+    Public ReadOnly Property Name As String Implements Q_U_U_U_Q_I.Name
+        Get
+            Return "WindowedForm"
+        End Get
+    End Property
+
+    Public Sub ExecuteDebug(mainForm As Form1) Implements Q_U_U_U_Q_I.ExecuteDebug
+        Form1 = mainForm
+    End Sub
+
+    Public Sub ExecuteUISubs(mainForm As UI) Implements Q_U_U_U_Q_I.ExecuteUISubs
+        UI = mainForm
+    End Sub
+
+    Public WithEvents WindowedFormTimer As New Timer With {.Interval = 1000, .Enabled = True}
+
+    Public Sub WindowedFormTimer_Tick(sender As Object, e As EventArgs) Handles WindowedFormTimer.Tick
+        If Form1.currentForm IsNot Nothing Then
+            If Form1.currentForm.FormBorderStyle = FormBorderStyle.None Then
+                Form1.currentForm.FormBorderStyle = FormBorderStyle.Sizable
+            End If
+        End If
+    End Sub
+End Class
---
+++ WindowedForm/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowedForm")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("SBFC Group")>
+<Assembly: AssemblyProduct("WindowedForm")>
+<Assembly: AssemblyCopyright("Copyright © SBFC Group 2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9c7da53-9f75-493b-974d-2f5052c41a23")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
